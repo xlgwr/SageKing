2721de5686f2d02756226c48122ac498fe7c443e
xlgwr <xlgwr@163.com>
2024-06-14 22:55:41 +0800

    update:整理界面

--- src/SageKing.Studio/globalUsings.cs
+++ src/SageKing.Studio/globalUsings.cs
@@ -1,14 +1,14 @@
-﻿global using Microsoft.Extensions.DependencyInjection;
-global using System.Collections.Concurrent;
-
+﻿global using System.Collections.Concurrent;
 global using System.ComponentModel.DataAnnotations;
+global using Microsoft.Extensions.DependencyInjection;
 
 global using SageKing.Core.Contracts;
-global using SageKing.Core.EventMessage;
 global using SageKing.Core.Extensions;
+global using SageKing.Core.EventMessage;
 
 global using IceRpc;
 global using MediatR;
 global using SageKing.IceRPC;
 global using SageKing.Extensions;
+global using SageKing.Studio.Data;
 global using SageKing.Studio.Validations;
