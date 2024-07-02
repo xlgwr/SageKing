c8a77ce7ed581f207764f1aa6a30535024b0b092
xlgwr <xlgwr@163.com>
2024-07-02 22:51:56 +0800

    update:RequiredListAttribute变移到基础库中

---
+++ src/SageKing.Core/Attributes/Validation/RequiredListAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Humanizer.Localisation;
+using System.ComponentModel.DataAnnotations;
+
+namespace SageKing.Core.Attributes;
+
+public class RequiredListAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value == null)
+        {
+            return false;
+        } 
+
+        //list
+        var getCount = value.GetType().GetProperty("Count");
+        if (getCount != null && (int)getCount.GetValue(value) <= 0)
+        {
+            return false;
+        }
+
+        //array[]
+        var getLengthProperty = value.GetType().GetProperty("Length");
+        if (getLengthProperty != null && (int)getLengthProperty.GetValue(value) <= 0)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+}
--- src/SageKing.Studio/globalUsings.cs
+++ src/SageKing.Studio/globalUsings.cs
@@ -15,7 +15,6 @@ global using MediatR;
 global using SageKing.IceRPC;
 global using SageKing.Extensions;
 global using SageKing.Studio.Data;
-global using SageKing.Studio.Validations;
 global using SageKing.IceRPC.Client.Options;
 global using SageKing.IceRPC.Server.Options;
 
