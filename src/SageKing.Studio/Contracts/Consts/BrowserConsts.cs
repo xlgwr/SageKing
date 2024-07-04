e38e7e295ab2a7a0b28f8b69fe36faaa6befe412
xlgwr <xlgwr@163.com>
2024-07-04 23:29:08 +0800

    update

--- src/SageKing.Studio/Contracts/Consts/BrowserConsts.cs
+++ src/SageKing.Studio/Contracts/Consts/BrowserConsts.cs
@@ -1,4 +1,6 @@
-﻿namespace SageKing.Studio.Contracts;
+﻿using BlazorPro.BlazorSize;
+
+namespace SageKing.Studio.Contracts;
 
 public sealed class BrowserConsts
 {
@@ -7,4 +9,7 @@ public sealed class BrowserConsts
     public const int browserHeight = 700;
     public const int browserHeight1080 = 924;
     public const int ScrollYOffset = 280;
+
+
+    public static string LargeHeight1080_800 = Breakpoints.Between("(max-Height: 1080px)", "(min-Height: 800px)");
 }
