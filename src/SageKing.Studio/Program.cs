98fa1d51936022d664af9eeaf23f04a895b5c8d8
xlgwr <xlgwr@163.com>
2024-07-10 00:31:27 +0800

    update

--- src/SageKing.Studio/Program.cs
+++ src/SageKing.Studio/Program.cs
@@ -63,7 +63,7 @@ app.UseSageKing();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler();
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
